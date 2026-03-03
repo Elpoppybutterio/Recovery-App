014d06cab232811c77d385b048381e4af1dab89f
Jason <jason.lehman@flippos.com>
2026-03-02 18:35:44 -0700

    [v0.9.9] Fix Big Book 60-63 syntax block

--- apps/mobile/lib/routines/bigBookTexts.ts
+++ apps/mobile/lib/routines/bigBookTexts.ts
@@ -42,10 +42,14 @@ export const BIG_BOOK_86_88_READ_TEXT = [
   ].join("\n"),
 ].join("\n\n");
 
-export const BIG_BOOK_60_63_READ_TEXT = [
-  "The first requirement is that we be convinced that any life run on self-will can hardly be a success.",
-  "Most people try to live by self-propulsion. Each person is like an actor who wants to run the whole show.",
-  "Selfishness, self-centeredness: that, we think, is the root of our troubles.",
-  "We decided that hereafter in this drama of life, God was going to be our Director.",
-  "When we sincerely took such a position, all sorts of remarkable things followed. We were reborn.",
-].join("\n\n");
+export const BIG_BOOK_60_63_READ_TEXT = `
+The first requirement is that we be convinced that any life run on self-will can hardly be a success.
+
+Most people try to live by self-propulsion. Each person is like an actor who wants to run the whole show.
+
+Selfishness, self-centeredness: that, we think, is the root of our troubles.
+
+We decided that hereafter in this drama of life, God was going to be our Director.
+
+When we sincerely took such a position, all sorts of remarkable things followed. We were reborn.
+`.trim();
